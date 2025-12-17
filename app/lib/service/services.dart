1f963086156509c5a5349b84b85573a77c568db5
Istvan Soos <istvan.soos@gmail.com>
2025-12-17 14:55:17 +0100

    ignore all

--- app/lib/service/services.dart
+++ app/lib/service/services.dart
@@ -259,12 +259,8 @@ Future<R> _withPubServices<R>(FutureOr<R> Function() fn) async {
         try {
           await PrimaryDatabase.tryRegisterInScope();
         } catch (e, st) {
-          if (envConfig.isRunningInAppengine) {
-            // ignore setup issues for now
-            _logger.warning('Could not connect to Postgresql database.', e, st);
-          } else {
-            rethrow;
-          }
+          // ignore setup issues for now
+          _logger.warning('Could not connect to Postgresql database.', e, st);
         }
 
         await setupCache();
